f787191a94dd9d245c055aa50a5d405bf5fe6e67
nfoj <nfoj_@hotmail.com>
2024-12-09 20:40:29 -0300

    update

--- cursos/rust/rust-study/src/main.rs
+++ cursos/rust/rust-study/src/main.rs
@@ -1,35 +1,27 @@
 // cargo watch -x run
-//mod ssemantics_a1;
-//mod ssemantics_a2;
-//mod ssemantics_a3;
-//mod ssemantics_a4;
-//mod ssemantics_a5;
+//mod a1_ssemantics;
+//mod a2_ssemantics;
+//mod a3_ssemantics;
+//mod a4_ssemantics;
+//mod a5_ssemantics;
 //mod hr;
 
 fn main() {
-    //ssemantics_a1::a1();
-    //ssemantics_a2::a2();
-    //ssemantics_a3::a3();
-    //ssemantics_a4::a4();
-    //ssemantics_a5::a5();
+    //a1_ssemantics::a1();
+    //a2_ssemantics::a2();
+    //a3_ssemantics::a3();
+    //a4_ssemantics::a4();
+    //a5_ssemantics::a5();
     //hr::hackerrank();
-    study();
+    name();
+
+    println!("{:?}", name("Mashu", "kyrielight"));
+
+    let result: String = name("Mashu", "kyrielight");
+    println!("{:?}", result);
 }
 
-fn study() {
-    println!("---------------------");
-
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
-    println!("---------------------");
+fn name(first: &str, last: &str) -> String {
+    let full_name = format!("{0} {1}", first, last);
+    return full_name;
 }
