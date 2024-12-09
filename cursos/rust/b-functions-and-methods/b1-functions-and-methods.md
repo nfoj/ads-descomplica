bd19e78958e2ddba13b4cfd3243d14b0b8a4fa8c
nfoj <nfoj_@hotmail.com>
2024-12-09 11:54:23 -0300

    update + rename

---
+++ cursos/rust/b-functions-and-methods/b1-functions-and-methods.md
@@ -0,0 +1,35 @@
+# Rust
+
+3. Functions and Methods
+
+a. Functions;
+
+- Functions: reusable code blocks that perform specific tasks;
+
+```
+
+  // 
+  fn main () {}
+  
+```
+
+- fn = function
+- main = name
+- () = specifications
+- {} = code
+
+
+- Example:
+
+```
+
+  fn main () {
+    study();
+  }
+  
+  fn study () {}
+  
+```
+
+
+
--- cursos/rust/rust-study/src/main.rs
+++ cursos/rust/rust-study/src/main.rs
@@ -3,7 +3,7 @@
 //mod ssemantics_a2;
 //mod ssemantics_a3;
 //mod ssemantics_a4;
-mod ssemantics_a5;
+//mod ssemantics_a5;
 //mod hr;
 
 fn main() {
@@ -11,6 +11,25 @@ fn main() {
     //ssemantics_a2::a2();
     //ssemantics_a3::a3();
     //ssemantics_a4::a4();
-    ssemantics_a5::a5();
+    //ssemantics_a5::a5();
     //hr::hackerrank();
+    study();
+}
+
+fn study() {
+    println!("---------------------");
+
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
+    println!("---------------------");
 }
